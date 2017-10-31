2cf58486ff24c0781ee283ef345f590d9125eb21
deb <debkbanerji@gmail.com>
2017-10-30 21:28:01 -0400

    Update Logo

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+    selector: 'app-about',
+    templateUrl: './about.component.html',
+    styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+    public LOGO_URL: string;
 
-  ngOnInit() {
-  }
+    constructor() {
+    }
+
+    ngOnInit() {
+        this.LOGO_URL = '/assets/images/logo.png';
+    }
 
 }
