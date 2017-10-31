273392f29cbd8a1c1c96e6c2c2772f3f1449398e
deb <debkbanerji@gmail.com>
2017-10-31 13:37:52 -0400

    Switch to mediaDevices

--- src/assets/js/audio-main.js
+++ src/assets/js/audio-main.js
@@ -8,16 +8,13 @@ let recorder = null;
 let audioInterface = {};
 
 audioInterface.initialize = function (successCallback, errorCallback) {
-    // audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-    // gainNode = audioCtx.createGain();
     audioBlob = null;
     recorder = null;
-    if (navigator.getUserMedia) {
-        navigator.getUserMedia(
+    if (navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices.getUserMedia(
             {
                 audio: true
-            },
-
+            }).then(
             // Success callback
             function (stream) {
                 const source = audioCtx.createMediaStreamSource(stream);
@@ -36,24 +33,14 @@ audioInterface.initialize = function (successCallback, errorCallback) {
                     audioBlob = blob;
                 };
 
-            },
-
+            }).catch(
             // Error callback
             function (err) {
                 console.log('The following gUM error occured: ' + err);
-                errorCallback('Unable to record audio due to insufficient permissions. In order to post, try granting this page microphone permissions or accessing the application over https');
-                // console.log(errorCallback);
-
-                // if (errorCallback) {
-                //     errorCallback();
-                // }
-            }
-        );
+                errorCallback('Unable to record audio due to insufficient permissions. In order to post, try granting this page microphone permissions or accessing it over https.');
+            });
     } else {
-        // if (errorCallback) {
-        //     errorCallback();
-        // }
-        console.log('getUserMedia not supported on your browser!');
+        console.log('navigator.mediaDevices.getUserMedia not supported on your browser!');
         errorCallback('Media recording not supported on this browser. Try using a supported browser, such as Chrome.')
     }
 };
